6739d77e91673e34e18acc6bc5da4c741bc66798
Jan Garaj <jan.garaj@gmail.com>
2024-12-25 14:33:35 +0000

    Dashboard FSx update

--- .github/workflows/standards.py
+++ .github/workflows/standards.py
@@ -23,7 +23,8 @@ validated_dashboards = [
    "aws-elb/aws-elb-application-lb.json",
    "aws-elb/aws-elb-classic-lb.json",
    "aws-emr/aws-emr-hadoop-2.json",
-   "aws-eventbridge/aws-eventbridge.json"
+   "aws-eventbridge/aws-eventbridge.json",
+   "aws-fsx/aws-fsx.json",
 ]
 
 standard_footer = "<a target=\"_blank\" href=\"http://www.monitoringartist.com\" title=\"Dashboard maintained by Monitoring Artist - DevOps / Docker / Kubernetes / AWS ECS / Google GCP / Zabbix / Zenoss / Terraform / Monitoring\"><img src=\"https://monitoringartist.github.io/monitoring-artist-logo-grafana.png\" height=\"30px\" /></a> | \n<a target=\"_blank\" href=\"https://docs.aws.amazon.com/transfer/latest/userguide/monitoring.html\">AWS CloudWatch Transfer Family documentation</a> | \n<a target=\"_blank\" href=\"https://grafana.com/dashboards/20008\">Installed from Grafana.com dashboards</a>"
