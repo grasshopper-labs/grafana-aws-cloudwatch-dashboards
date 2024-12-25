f64b71da87bc2dab9e667d799978766d13363702
Jan Garaj <jan.garaj@gmail.com>
2024-12-25 16:37:15 +0000

    Dashboard Logs update

--- .github/workflows/standards.py
+++ .github/workflows/standards.py
@@ -29,6 +29,7 @@ validated_dashboards = [
    "aws-kinesis/aws-kinesis.json",
    "aws-kinesis-firehose/aws-kinesis-firehose.json",
    "aws-lambda/aws-lambda.json",
+   "aws-logs/aws-logs.json",
 ]
 
 standard_footer = "<a target=\"_blank\" href=\"http://www.monitoringartist.com\" title=\"Dashboard maintained by Monitoring Artist - DevOps / Docker / Kubernetes / AWS ECS / Google GCP / Zabbix / Zenoss / Terraform / Monitoring\"><img src=\"https://monitoringartist.github.io/monitoring-artist-logo-grafana.png\" height=\"30px\" /></a> | \n<a target=\"_blank\" href=\"https://docs.aws.amazon.com/transfer/latest/userguide/monitoring.html\">AWS CloudWatch Transfer Family documentation</a> | \n<a target=\"_blank\" href=\"https://grafana.com/dashboards/20008\">Installed from Grafana.com dashboards</a>"
